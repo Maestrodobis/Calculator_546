15d6846e556d6bf952438ac462792ea03a6a4edf
Maestrodobis <imklein94@gmail.com>
2016-10-12 23:11:26 -0400

    updated calculator for end of sprint 1

--- Calculator.java
+++ Calculator.java
@@ -1,34 +1,63 @@
-
 public class Calculator 
 {
-	public static double add(double a, double b) 
+	public double add(double a, double b) 
 	{
 	    return a+b;          
 	}
 
-	public static double subtract(double a, double b) 
+	public double subtract(double a, double b) 
 	{
 	    return a-b;          
 	}
 
-	public static double multiply(double a, double b) 
+	public double multiply(double a, double b) 
 	{
 	    return a*b;       
 	}
 
-	public static double divide(double a, double b) 
+	public double divide(double a, double b) 
 	{
 	    return a/b;          
 	}
 
-	public static double power(double a, double b)
+	public static void main(String args[])
 	{
-		double result =a;
-
-	    for (int x=2; x<=b; x++)
-	    	result *= a;
-
-	    return result;
+		Calculator calc = new Calculator();
+		double number1 = 0;
+		double number2 = 0;
+		
+		if(args.length == 3)
+		{	
+			try {
+	            // Parse the string argument into an integer value.
+				number1 = Double.parseDouble(args[2]);
+	        }
+	        catch (NumberFormatException nfe) {
+	            // The second argument isn't a valid double.  Print
+	            // an error message, then exit with an error code.
+	            System.out.println("The first number must be a double.");
+	            System.exit(1);
+	        }
+			try {
+	            // Parse the string argument into an integer value.
+				number2 = Double.parseDouble(args[3]);
+	        }
+	        catch (NumberFormatException nfe) {
+	            // The third argument isn't a valid double.  Print
+	            // an error message, then exit with an error code.
+	            System.out.println("The second number must be a double.");
+	            System.exit(1);
+	        }
+			
+			System.out.println(calc.add(number1, number2));
+			System.out.println(calc.subtract(number1, number2));
+			System.out.println(calc.multiply(number1, number2));
+			System.out.println(calc.divide(number1, number2));
+		}
+		else
+		{
+			System.out.println("USAGE: ./calculator.java <double> <double>");
+			System.exit(0);
+		}
 	}
-
 }
